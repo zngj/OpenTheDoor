8f0729e19a0e2441f4e5f4e372e143b3f4c01cda
Feynman <codingmonkey83@gmail.com>
2017-08-07 13:52:13 +0800

    (1)加入协议的解析 示例代码

---
+++ gate/ProtocolTest/ProtocolTest/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ProtocolTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            byte[] data = new byte[32+128];
+
+            //产生一些随机数据当消息
+            Random rand = new Random();
+            rand.NextBytes(data);
+
+
+            Protocol pt = new Protocol();
+
+            byte[] sndData = pt.encap(data,data.Length);
+
+
+            //写入到文件中去
+
+            string filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "test.bin");
+            FileStream fs = new FileStream(filePath, FileMode.Append);
+
+
+            fs.Write(sndData, 0, sndData.Length);
+
+            fs.Close();
+
+            MessageBox.Show("发送OK");
+
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "test.bin");
+            FileStream fs = new FileStream(filePath, FileMode.Open);
+
+            Protocol pt = new Protocol();
+
+            byte[] data = new byte[16];
+
+            while (true)
+            {
+               int length= fs.Read(data, 0, data.Length);
+
+               if (length > 0)
+               {
+                   pt.parse(data, length);
+               }
+               if (length < data.Length)
+               {
+                   break;
+               }
+            }
+
+            fs.Close();
+
+        }
+    }
+}
---
+++ gate/ProtocolTest/ProtocolTest/Protocol.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProtocolTest
+{
+    class Protocol
+    {
+
+        int frameLength = 0;
+
+        byte[] frameBuffer=new byte[64*1024];
+
+        bool met10 = false;
+
+
+        /// <summary>
+        /// 解析数据
+        /// </summary>
+        /// <param name="data"></param>
+        /// <param name="length"></param>
+        public void parse(byte[] data, int length)
+        {
+            for (int i = 0; i < length; i++)
+            {
+                switch (data[i])
+                {
+                    case 0x10:
+                        {
+                            if (frameLength == 0)
+                            {
+                                frameBuffer[frameLength++] = 0x10;
+
+                            }
+                            else if (frameLength > 1)
+                            { 
+                                if(!met10)
+                                {
+                                    frameBuffer[frameLength++] = 0x10;
+                                }
+                            }
+
+                            met10 = !met10;
+                        }
+                        break;
+                    case 0x02:
+                        {
+                            if (met10) //met a frame header
+                            {
+                                frameBuffer[0] = 0x10;
+                                frameBuffer[1] = 0x02;
+                                frameLength = 2;
+                               
+                            }
+                            else if (frameLength > 1)
+                            {
+                                frameBuffer[frameLength++] = 0x02;
+                            }
+
+                            met10 = false;
+
+                        }
+                        break;
+                    case 0x03:
+                        {
+                            if (met10) //met a frame tailer
+                            {
+                                frameBuffer[frameLength++] = 0x03;
+
+                                //做长度和校验和验证
+
+                                if (frameLength >=8) //2(帧头)+2(长度)+N(数据)+2(校验和)+2(帧尾)
+                                {
+                                    UInt16 fLen = BitConverter.ToUInt16(frameBuffer, 2);
+
+                                    ushort sum = 0;
+                                    if (fLen == (frameLength - 2 - 2 - 2))//做长度的校验
+                                    {
+                                        sum = 0;
+                                        for (int j = 0; j < fLen - 2; j++)
+                                        {
+                                            sum += frameBuffer[4 + j];
+                                        }
+
+                                        byte checkLow = frameBuffer[2 + fLen];
+                                        byte checkHigh = frameBuffer[2 + fLen + 1];
+
+                                        if (((sum & 0xff) == checkLow) && ((sum >> 8) & 0xff) == checkHigh) //校验和通过
+                                        {
+                                            //这个时候，数据帧完全OK
+
+                                            Console.WriteLine(" message OK");
+                                        }
+
+                                    }
+                                }
+                                this.frameLength = 0; //开始新的一帧的获取
+                               
+                            }
+                            else if (frameLength > 1)
+                            {
+                                frameBuffer[frameLength++] = 0x03;
+                            }
+                            met10 = false;
+                        }
+                        break;
+                    default:
+                        {
+                            if (frameLength > 1)
+                            {
+                                frameBuffer[frameLength++] = data[i];
+                            }
+                        }
+                        break;
+                }
+            }
+        }
+
+
+
+        /// <summary>
+        /// 封装发送的数据
+        /// </summary>
+        /// <param name="data">这个就是要发送的消息(消息头+消息体)</param>
+        /// <param name="length"></param>
+        /// <returns></returns>
+        public byte[] encap(byte[] data, int length)
+        {
+            List<byte> listSnd=new List<byte>();
+
+
+            //计算和校验
+
+            UInt16 sum = 0;
+
+            for (int i = 0; i < length; i++)
+            {
+                sum += data[i];
+            }
+
+            UInt16 fLen = (UInt16)(length + 2);
+
+            //帧头
+
+            listSnd.Add(0x10);
+            listSnd.Add(0x02);
+
+            //长度
+            byte lenLow = (byte)(fLen & 0xff);
+            listSnd.Add(lenLow);
+            if (lenLow == 0x10)
+            {
+                listSnd.Add(0x10);
+            }
+            byte lenHigh = (byte)((fLen>>8) & 0xff);
+            listSnd.Add(lenHigh);
+            if (lenHigh == 0x10)
+            {
+                listSnd.Add(0x10);
+            }
+
+            //数据
+
+            for (int i = 0; i < length; i++)
+            {
+                listSnd.Add(data[i]);
+                if (data[i] == 0x10)
+                {
+                    listSnd.Add(0x10);
+                }
+            }
+
+            //校验和
+
+            byte sumLow = (byte)(sum & 0xff);
+            listSnd.Add(sumLow);
+            if (sumLow == 0x10)
+            {
+                listSnd.Add(0x10);
+            }
+            byte sumHigh = (byte)((sum >> 8) & 0xff);
+            listSnd.Add(sumHigh);
+            if (sumHigh == 0x10)
+            {
+                listSnd.Add(0x10);
+            }
+
+            //帧尾
+
+            listSnd.Add(0x10);
+            listSnd.Add(0x03);
+
+            return listSnd.ToArray();
+        }
+
+
+    }
+}

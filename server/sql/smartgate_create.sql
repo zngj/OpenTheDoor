cf8f76ec9045f65b17489780c06e45697bf6935c
carsonsx <carsonsx@qq.com>
2017-08-10 09:52:45 +0800

    rsa and sql

--- server/sql/smartgate_create.sql
+++ server/sql/smartgate_create.sql
@@ -11,7 +11,7 @@
  Target Server Version : 50719
  File Encoding         : 65001
 
- Date: 06/08/2017 22:22:36
+ Date: 10/08/2017 04:29:03
 */
 
 SET NAMES utf8mb4;
@@ -34,15 +34,24 @@ CREATE TABLE `sg_codec_key` (
 -- ----------------------------
 DROP TABLE IF EXISTS `sg_gate_info`;
 CREATE TABLE `sg_gate_info` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `city_code` varchar(2) COLLATE utf8mb4_bin NOT NULL,
-  `city_name` varchar(20) COLLATE utf8mb4_bin NOT NULL,
-  `station_code` varchar(4) COLLATE utf8mb4_bin NOT NULL,
+  `id` varchar(10) COLLATE utf8mb4_bin NOT NULL,
+  `direction` tinyint(1) NOT NULL COMMENT '0-入;1-出',
+  `station_id` varchar(10) COLLATE utf8mb4_bin NOT NULL,
   `station_name` varchar(10) COLLATE utf8mb4_bin NOT NULL,
-  `gate_code` varchar(6) COLLATE utf8mb4_bin NOT NULL,
-  `direction` varchar(3) COLLATE utf8mb4_bin NOT NULL COMMENT 'in-入;out-出',
-  PRIMARY KEY (`id`)
+  `city_id` varchar(10) COLLATE utf8mb4_bin NOT NULL,
+  `city_name` varchar(20) COLLATE utf8mb4_bin NOT NULL,
+  PRIMARY KEY (`id`) USING BTREE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+-- ----------------------------
+-- Records of sg_gate_info
+-- ----------------------------
+BEGIN;
+INSERT INTO `sg_gate_info` VALUES ('010100101', 0, '0101001', '五一广场', '0101', '长沙');
+INSERT INTO `sg_gate_info` VALUES ('010100102', 1, '0101001', '王一广场', '0101', '长沙');
+INSERT INTO `sg_gate_info` VALUES ('010100201', 0, '0101002', '黄兴广场', '0101', '长沙');
+INSERT INTO `sg_gate_info` VALUES ('010100202', 1, '0101002', '黄兴广场', '0101', '长沙');
+COMMIT;
 
 -- ----------------------------
 -- Table structure for sg_router_evidence
@@ -51,10 +60,10 @@ DROP TABLE IF EXISTS `sg_router_evidence`;
 CREATE TABLE `sg_router_evidence` (
   `evidence_id` varchar(32) COLLATE utf8mb4_bin NOT NULL,
   `user_id` varchar(32) COLLATE utf8mb4_bin NOT NULL,
-  `type` smallint(1) NOT NULL DEFAULT 0 COMMENT '0-通用;1-入阐;2-出阐',
+  `direction` smallint(1) NOT NULL DEFAULT '0' COMMENT '0-入阐;1-出阐;3-通用',
   `create_time` datetime NOT NULL,
   `expires_at` datetime NOT NULL,
-  `status` smallint(1) NOT NULL COMMENT '1-已下发;2-已使用;3-已过期',
+  `status` tinyint(1) NOT NULL COMMENT '1-已下发;2-已使用;3-已过期',
   `used_time` datetime DEFAULT NULL COMMENT '使用时间',
   PRIMARY KEY (`evidence_id`) USING BTREE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
@@ -66,19 +75,34 @@ DROP TABLE IF EXISTS `sg_router_info`;
 CREATE TABLE `sg_router_info` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
   `user_id` varchar(128) COLLATE utf8mb4_bin NOT NULL,
-  `in_time` datetime DEFAULT NULL,
-  `in_ evidence` varchar(32) COLLATE utf8mb4_bin DEFAULT NULL,
-  `in_station_code` varchar(4) COLLATE utf8mb4_bin DEFAULT NULL,
+  `in_station_id` varchar(10) COLLATE utf8mb4_bin DEFAULT NULL,
   `in_station_name` varchar(20) COLLATE utf8mb4_bin DEFAULT NULL,
-  `in_gate_code` varchar(6) COLLATE utf8mb4_bin DEFAULT NULL,
-  `out_time` datetime DEFAULT NULL,
-  `out_ evidence` varchar(32) COLLATE utf8mb4_bin DEFAULT NULL,
-  `out_station_code` varchar(4) COLLATE utf8mb4_bin DEFAULT NULL,
+  `in_gate_id` varchar(10) COLLATE utf8mb4_bin DEFAULT NULL,
+  `in_evidence` varchar(32) COLLATE utf8mb4_bin DEFAULT NULL,
+  `in_time` datetime DEFAULT NULL,
+  `out_station_id` varchar(10) COLLATE utf8mb4_bin DEFAULT NULL,
   `out_station_name` varchar(20) COLLATE utf8mb4_bin DEFAULT NULL,
-  `out_gate_code` varchar(6) COLLATE utf8mb4_bin DEFAULT NULL,
-  `status` tinyint(1) NOT NULL COMMENT '1-进行中;2-结束;3-无出站异常;4-无入站异常',
+  `out_gate_id` varchar(10) COLLATE utf8mb4_bin DEFAULT NULL,
+  `out_evidence` varchar(32) COLLATE utf8mb4_bin DEFAULT NULL,
+  `out_time` datetime DEFAULT NULL,
+  `money` decimal(4,2) DEFAULT NULL,
+  `status` tinyint(1) NOT NULL COMMENT '1-入闸;2-出闸;3-出闸(无入闸);4-未出闸(异常);5-未入闸(异常)',
   `exception_time` datetime DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+-- ----------------------------
+-- Table structure for sg_sys_notification
+-- ----------------------------
+DROP TABLE IF EXISTS `sg_sys_notification`;
+CREATE TABLE `sg_sys_notification` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` varchar(32) COLLATE utf8mb4_bin NOT NULL,
+  `category` varchar(10) COLLATE utf8mb4_bin NOT NULL,
+  `content_id` varchar(32) COLLATE utf8mb4_bin NOT NULL,
+  `received` tinyint(1) NOT NULL DEFAULT '0',
+  `insert_time` datetime NOT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 
 -- ----------------------------
